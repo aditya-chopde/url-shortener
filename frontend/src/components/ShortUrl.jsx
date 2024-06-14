e4335377432ba7188b690dbde43c8941325dbafc
Aditya Chopde <its.adityac@gmail.com>
2024-06-14 17:55:59 +0530

    Setup tailwind in the frontend folder

---
+++ frontend/src/components/ShortUrl.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ShortUrl = () => {
+  return (
+    <>
+    <div>
+        <div className='bg-[#00ED64] w-full'>
+            <h1>Enter/Paste link here: </h1>
+        </div>
+    </div> 
+    </>
+  )
+}
+
+export default ShortUrl
